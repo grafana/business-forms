d21bf70f2f6814c0e150272958048f149f5a018d
Brian Gann <bkgann@gmail.com>
2026-03-12 22:34:29 -0400

    Fix legacy scenes router resolution

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,31 @@
+import path from 'path';
 import type { Configuration } from 'webpack';
+import { NormalModuleReplacementPlugin } from 'webpack';
 import { merge } from 'webpack-merge';
 import grafanaConfig, { Env } from './.config/webpack/webpack.config';
 
 const config = async (env: Env): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
+  const legacyScenesPath = path.join(
+    'node_modules',
+    '@volkovlabs',
+    'components',
+    'node_modules',
+    '@grafana',
+    'scenes'
+  );
+  const legacyReactRouterDomPath = path.resolve(__dirname, 'node_modules/@grafana/ui/node_modules/react-router-dom');
 
-  return merge(baseConfig, {});
+  return merge(baseConfig, {
+    plugins: [
+      // @volkovlabs/components vendors @grafana/scenes v5, which expects the React Router v5 Switch API.
+      new NormalModuleReplacementPlugin(/^react-router-dom$/, (resource) => {
+        if (resource.context.includes(legacyScenesPath)) {
+          resource.request = legacyReactRouterDomPath;
+        }
+      }),
+    ],
+  });
 };
 
 export default config;
