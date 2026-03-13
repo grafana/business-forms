a1bacd03b3439fbdfb821c5fdad292e3aff0b68e
Brian Gann <bkgann@gmail.com>
2026-03-12 22:29:28 -0400

    Remove bundle analyzer from webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,11 @@
-// webpack.config.ts
 import type { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import grafanaConfig from './.config/webpack/webpack.config';
+import grafanaConfig, { Env } from './.config/webpack/webpack.config';
 
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
-/**
- * Config
- */
-const config = async (env): Promise<Configuration> => {
+const config = async (env: Env): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
 
-  return merge(baseConfig, {
-    output: {
-      asyncChunks: true,
-    },
-    resolve: {
-      plugins: [new TsconfigPathsPlugin()],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: '../LICENSE-original', to: '.' },
-          { from: '../NOTICES.md', to: '.' },
-        ],
-      }),
-    ],
-  });
+  return merge(baseConfig, {});
 };
 
 export default config;
